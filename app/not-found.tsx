69c16a1454985b3ffd5e3e229de9ccb09167d56c
Shubham Chaturvedi <19shubham11@gmail.com>
2024-06-27 23:53:48 +0200

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,8 @@ export default function NotFound() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="mb-4">Now that's a name I haven't heard in a long time.</p>
+      <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmxybTBzaWhrenUxamRiNmdjYXIxNnZnOGR2NHR0cXhtYXR1MWQ0aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Bp3dFfoqpCKFyXuSzP/giphy.webp"></img>
     </section>
   )
 }
